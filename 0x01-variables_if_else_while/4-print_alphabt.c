633eea9a320e6202dd85295c6df7b1235182421d
lawal olatunji <lawolatunji@gmail.com>
2023-02-17 15:21:40 -0800

    alphabet soup,

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Print alphabet in lowercase except q and e.
+ * Return: 0
+ */
+int main(void)
+{
+char letterlower;
+/* print lowercase letters minus q and e */
+for (letterlower = 'a'; letterlower <= 'z'; letterlower++)
+{
+if (lowercase != q && lowercase != e)
+putchar(letterlower);
+}
+putchar('\n');
+return (0);
+}
