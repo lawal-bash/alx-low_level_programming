8f0d38103247ccab4623e6ddff86cb26d6e3c7e9
lawal olatunji <lawolatunji@gmail.com>
2023-02-17 15:52:15 -0800

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Print single digit number of base 10 from 0 using putchar.
+ * Return: 0
+ */
+int main(void)
+{
+int num;
+for (num = 0; num < 10; num++)
+putchar((num % 10) + '0');
+putchar('\n');
+return (0);
+}
