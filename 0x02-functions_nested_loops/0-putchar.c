dee837a11552c993406d8667f5fd383e51174ed5
lawal olatunji <lawolatunji@gmail.com>
2023-02-21 04:33:29 -0800

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,18 @@
-#include <stdlib.h>
+#inclue "main.h"
 /**
  * main - A program that print _putchar followed by a newline
  * Return: 0
  */
 int main(void)
 {
-putchar("_putchar");
-putchar("\n");
+_putchar("_");
+_putchar("p");
+_putchar("u");
+_putchar("t");
+_putchar("c");
+_putchar("h");
+_putchar("a");
+_putchar("r");
+_putchar("\n");
 return (0);
 }
