000fabd8ebb3c75cf76a82508aa1a528ce4d6893
lawal olatunji <lawolatunji@gmail.com>
2023-02-21 22:48:29 -0800

    6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - computes the abs value of an int
+ * @n: the int in the code
+ * * Return: the abs of the int
+ */
+int _abs(int n)
+{
+if (n > 0)
+return (n);
+else
+return (-n);
+}
