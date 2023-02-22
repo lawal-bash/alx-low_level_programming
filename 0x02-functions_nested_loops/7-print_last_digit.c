86d894e9c697f458f51f9fe03fc41ff9b3864830
lawal olatunji <lawolatunji@gmail.com>
2023-02-21 23:20:08 -0800

    7-print_last_digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,17 +1,14 @@
 #include "main.h"
 /**
- * print_last_digit - computes the abs value of an int
- * @int: expected varaible type
- * Return: the the value of last digit
+ * print_last_digit - Computes the abs value of an int
+ * @n: the variable that is defined
+ * Return: the value of last digit
  */
-int print_last_digit(int)
+int print_last_digit(int n)
 {
-int num, digit;
-/* Asking for input */
-_putchar("Enter the digit: ");
-scanf("%d", &num);
-digit = num % 10;
-/* Displaying output */
-_putchar("Last Digit of %d is: %d", num, digit);
-return (0);
+int last_digit = n % 10;
+if (last_digit < 0)
+last_digit *= -1;
+_putchar(last_digit + '0');
+return (last_digit);
 }
