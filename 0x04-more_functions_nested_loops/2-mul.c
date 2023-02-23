44648b381902f2c3e5e795e8ed3bf64e9ae1e76a
lawal olatunji <lawolatunji@gmail.com>
2023-02-23 06:05:09 -0800

    10-print_triangle

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - to multiple two numbers
+ * @a: first number
+ * @b: second number
+ * Return: result of multiplication
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - to print numbers from 0 - 9
+ * Return: void
+ */
+void print_numbers(void)
+{
+char c;
+for (c = '0'; c <= 9; c++)
+{
+_putchar(c);
+}
+}
