bc54b81bc9404f7987abdd951e02177fb39effa4
lawal olatunji <lawolatunji@gmail.com>
2023-02-23 22:55:48 -0800

    10-print_triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,24 +6,23 @@
  */
 void print_triangle(int size)
 {
-int row, hashes, spaces;
-if (size <= 0)
+int hash, index;
+if (size > 0)
 {
+for (hash = 1; hash <= size; hash++)
+{
+for (index = size - hash; index > index--)
+{
+_putchar(' ');
+}
+for (index = 0; index < hash; index--)
+{
+_putchar('#')
+}
+if (hash == size)
+{
+continue;
+}
 _putchar('\n');
-}
-else
-{
-for (row = 1; row <= size; row++)
-{
-for (spaces = size - row; spaces >= 1; spaces--)
-{
-_putchar('');
-
-for (hashes = 1; hashes <= row; hashes++)
-{
-_putchar('#');
-}
-_putchar('\n');
-}
 }
 }
