7371d70e515cac0a8c2b02956deeec1da03f4098
lawal olatunji <lawolatunji@gmail.com>
2023-02-24 17:15:48 -0800

    2-largest_number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,16 +14,22 @@ if (a > b && b > c)
 largest = a;
 }
 else if (a > c && c > b)
+{
 largest = a;
+}
 else if (b > a && a > c)
 {
 largest = b;
 }
 else if (b > c && c > a)
+{
 largest = b;
+}
 else if (c > a && a > b)
+{
 largest = c;
-else
+}
+else if  (c > b && b > a)
 {
 largest = c;
 }
