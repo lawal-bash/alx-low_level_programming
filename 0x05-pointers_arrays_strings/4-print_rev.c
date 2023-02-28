e8a9a705972811f71dc54bc1fb22b06cbd2ec69d
lawal olatunji <lawolatunji@gmail.com>
2023-02-28 09:14:40 -0800

    4-print_rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_rev - to print a string in reverse, followed by newline
+ * @s: string to print
+ */
+void print_rev(char *s)
+{
+int a, b;
+b = 0;
+while (s[b] != '\0')
+b++;
+for (a = b - 1; a >= 0; a--)
+{
+_putchar([a]);
+}
+_putchar('\n');
+}
