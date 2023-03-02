2d7b42181cc8ef106bd0f170b2196df319f2d51a
lawal olatunji <lawolatunji@gmail.com>
2023-03-02 15:46:29 -0800

    0-strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,7 +14,7 @@ for (i = 0; dest[i] != '\0'; i++)
 	destlen++;
 for (i = 0; src[i] != '\0'; i++)
 	srclen++;
-for (i = 0; i <= srclen ; i++)
+for (i = 0; i <= srclen; i++)
 	dest[destlen + i] = src[i];
 	return (dest);
 }
