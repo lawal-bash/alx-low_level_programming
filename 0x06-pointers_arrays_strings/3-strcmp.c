6262e72454f4eb17bde9d91d2acabc38c5859467
lawal olatunji <lawolatunji@gmail.com>
2023-03-02 16:28:07 -0800

    3-strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,20 +7,17 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-int i;
-for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+while (((*s1 != '\0') && (*s2 != '\0')) && (*s1 == *s2))
 {
-if (s[i] != s2[i])
-{
-if (s1[i] < s2[i])
-{
-return (s1[i] - s2[i]);
-else if (s1[i] > s2[i])
-return (s1[i] - s2[i]);
+s1++;
+s2++;
 }
-else
+if (*s1 == *s2)
+{
 return (0);
 }
+else
+{
+return (*s1 - *s2);
 }
-return (dest);
 }
