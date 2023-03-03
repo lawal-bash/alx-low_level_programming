b15dd58e6025a0e26795a6c15ca26e1cefd1a5d1
lawal olatunji <lawolatunji@gmail.com>
2023-03-02 16:51:15 -0800

    2-strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -3,13 +3,19 @@
  * _strncpy - to concatenates two strings using n bytes
  * @dest: destination strings
  * @src: source strings
- * @n: number of character to be append.
+ * @n: lenght of integer.
  * Return: the destination string
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 int i;
-for (i = 0; i < n; i++)
-dest[i] = src[i];
+for (i = 0; i < n && *(src + i); i++)
+{
+*(dest + i) = *(src + i);
+}
+for (; i < n; i++)
+{
+*(dest + i) = '\0';
+}
 return (dest);
 }
