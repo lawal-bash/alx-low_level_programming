3d567e0c9ced64b148dfae4e657fbe7ccad7a050
lawal olatunji <lawolatunji@gmail.com>
2023-04-10 10:22:03 -0700

    0-read_textfile

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * read_textfile - Reading a text file and printing it to POSIX std output
+ * @filename: The pointer to file name
+ * @letters: the number of letters it should read and print
+ * Return: actual number of letters it can read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+ssize_t openFile, readFile, writeFile;
+char *buffer;
+
+if (filename == NULL)
+return (0);
+
+buffer = malloc(sizeof(char) * letters);
+if (buffer == NULL)
+return (0);
+
+openFile = open(filename, O_RDONLY);
+readFile = read(o, buffer, letters);
+writeFile = write(STDOUT_FILENO, buffer, r);
+
+if (openFile == -1 || readFile == -1 || writeFile == -1
+	|| writeFile != readFile)
+{
+free(buffer);
+return (0);
+}
+free(buffer);
+close(openFile);
+
+return (writeFile);
+}
