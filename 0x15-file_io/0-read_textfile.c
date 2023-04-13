a47e3d9a6c2ddc6cbccf210ab84bc72c800dfb20
lawal olatunji <lawolatunji@gmail.com>
2023-04-13 04:58:04 -0700

    0-read_textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,31 +3,29 @@
  * read_textfile - Reading a text file and printing it to POSIX std output
  * @filename: The pointer to file name
  * @letters: the number of letters it should read and print
- * Return: actual number of letters it can read and print 
+ * Return: actual number of letters it can read and print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-ssize_t openFile, readFile, writeFile;
+ssize_t of, rf, wf;
 char *buffer;
 
 if (filename == NULL)
 return (0);
-
 buffer = malloc(sizeof(char) * letters);
 if (buffer == NULL)
 return (0);
 
-openFile = open(filename, O_RDONLY);
-readFile = read(openFile, buffer, letters);
-writeFile = write(STDOUT_FILENO, buffer, r);
+of = open(filename, O_RDONLY);
+rf = read(of, buffer, letters);
+wf = write(STDOUT_FILENO, buffer, rf);
 
-if (openFile == -1 || readFile == -1 || writeFile == -1 || writeFile != readFile)
+if (of == -1 || rf == -1 || wf == -1 || wf != rf)
 {
 free(buffer);
 return (0);
 }
 free(buffer);
-close(openFile);
-
-return (openFile);
+close(of);
+return (wf);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,10 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <unistd.h>
 
 
 int _putchar(char c);
